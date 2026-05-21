f1f85227c7a3beccb9c66e95762708a796d976d9
Amirsamyar Mirzaei <ammirzae@c2r3p4.42vienna.com>
2026-05-21 11:39:42 +0200

    [V0.0.2] WIP

--- get_next_line.c
+++ get_next_line.c
@@ -1,33 +1,73 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ammirzae <ammirzae@student.42vienna.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/21 11:06:29 by ammirzae          #+#    #+#             */
+/*   Updated: 2026/05/21 11:33:30 by ammirzae         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#define BUFFER_SIZE 45
-
-void	call_again(void)
+#include "get_next_line.h"
+/*
+int	call_again(void)
 {
 	static int line;
 
 	line = 0;
 	printf("Line: %d:", line);
 	++line;	
+	return(line);
 }
 
+t_str_nodes	list_new(char *new_str)
+{
+	t_str_nodes list;
+
+	list = malloc (sizeof(t_str_nodes));
+	if(list == NULL)
+			return(NULL);
+
+
+}
+*/
+
+//read, malloc, free
+void get_next_line(int fd)
+{
+	int count;
+	char buffer[BUFFER_SIZE];
+	int read_check;
+	
+	count = 0;
+	while ((read_check = read(fd , buffer, BUFFER_SIZE)) != -1 && count < 30)
+	{
+			if(read_check == -1)
+			{
+				printf("read error!\n");
+				return ;
+			}
+			if(read_check == 0)
+				return ;
+
+			printf("count: %d, '%s'\n", count++, buffer);
+	}
+	
+	return;
+
+}
 
 int main(int argc, char **argv) 
 {
 	int i;
 	size_t count;
 	int fd;
-	char buff[BUFFER_SIZE];
-	
+
 	i = 1;
 	fd = open(argv[1], O_RDONLY);
-	while(i < 20)
-	{
-		call_again();
-		count = read(fd, buff, BUFFER_SIZE);
-		write(1, &buff, count);
-		i++;
-	}
+	get_next_line(fd);
+
+
 }
---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ammirzae <ammirzae@student.42vienna.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/21 11:01:06 by ammirzae          #+#    #+#             */
+/*   Updated: 2026/05/21 11:38:31 by ammirzae         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 42
+
+
+
+typedef struct s_list
+{
+	char *str;
+	struct s_list *next;
+
+}	t_str_nodes;
